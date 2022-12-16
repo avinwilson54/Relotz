db68d80ac35e17d2513f6c4b85b23080d8834016
Avin Wilson <avinwilson54@gmail.com>
2022-12-16 16:58:49 +0530

    Typography and component styles updated

--- assets/js/custom/main.js
+++ assets/js/custom/main.js
@@ -1,4 +1,30 @@
+document.getElementById("priceSwitch").addEventListener("change", () => {
+  let checked = document.getElementById("priceSwitch").checked;
+  let monthPrice = document.getElementsByClassName("cost-per-month");
+  let yearPrice = document.getElementsByClassName("cost-per-year");
+  if (checked) {
+    document.getElementById("billAnnual").style.fontWeight = "700";
+    document.getElementById("billMonth").style.fontWeight = "400";
+    for (let i = 0; i < monthPrice.length; i++) {
+      for (let j = 0; j < yearPrice.length; j++) {
+        monthPrice[i].style.display = "none";
+        yearPrice[j].style.display = "block";
+      }
+    }
+  } else {
+    document.getElementById("billAnnual").style.fontWeight = "400";
+    document.getElementById("billMonth").style.fontWeight = "700";
+    for (let i = 0; i < monthPrice.length; i++) {
+      for (let j = 0; j < yearPrice.length; j++) {
+        monthPrice[i].style.display = "block";
+        yearPrice[j].style.display = "none";
+      }
+    }
+  }
+});
+
 let accordion = document.getElementsByClassName("accordion-container");
+document.getElementById("billMonth").style.fontWeight = "700";
 
 for (let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function () {
